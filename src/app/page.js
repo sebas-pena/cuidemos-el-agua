53ed3bcd878874fc46d03bd7f875fcdd51554a4b
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-02 16:02:57 -0300

    feat: Show the map

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
+import Map from "@/components/ui/Map";
+import LeaksCounter from "@/components/ui/cards/LeaksCounter";
+
 export default function Home() {
+  const height = typeof window !== "undefined" ? window.innerHeight - 68 : 0;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="relative overflow-hidden">
+      <Map height='calc(100vh - 68px)' />
+      <div className='absolute top-3 right-3 z-[9999999]'>
+        <LeaksCounter count={10} />
+      </div>
     </main>
   )
 }
