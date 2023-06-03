57a1076145090a49596f84c1d1d035f9ca859509
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-03 16:22:39 -0300

    feat: Use the button wrapper and the new reducer functions

--- src/components/ui/button/ReportButton.jsx
+++ src/components/ui/button/ReportButton.jsx
@@ -1,25 +1,32 @@
 'use client'
-import { hideCrosshair, showCrosshair } from '@/store/feature/MapSlice'
+import { cancelReport, startReport } from '@/store/feature/MapSlice'
 import Image from 'next/image'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import MapControlButton from './MapControlButton'
 
 const ReportButton = () => {
   const dispatch = useDispatch()
   const showingCrosshair = useSelector(state => state.map).showCrosshair
 
   const handleClick = () => {
-    showingCrosshair ? dispatch(hideCrosshair()) : dispatch(showCrosshair())
+    if (showingCrosshair) {
+      dispatch(cancelReport())
+    } else {
+      dispatch(startReport())
+    }
   }
 
   const styles = showingCrosshair
-    ? 'bg-neutral-600 hover:bg-neutral-700 active:bg-neutral-800'
-    : 'bg-red-600 hover:bg-red-700 active:bg-red-800'
+    ? 'neutral'
+    : 'red'
+
   return (
-    <button
-      className={`flex gap-2 w-full justify-center items-center rounded-lg  py-2 px-4 shadow-md ${styles}`}
+    <MapControlButton
       onClick={handleClick}
+      style={styles}
     >
+
       <Image src='/svg/report.svg' width={20} height={20} alt='Reportar' />
       <span className='font-coolvetica font-semibold text-xl text-white'>
         {
@@ -28,7 +35,7 @@ const ReportButton = () => {
             : 'Reportar Perdida'
         }
       </span>
-    </button>
+    </MapControlButton>
   )
 }
 
