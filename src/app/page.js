69a3ba27744fd06e009009f8819ceae4b2b4bf2a
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-03 01:57:51 -0300

    feat: Use the controls component

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,15 @@
 import dynamic from "next/dynamic";
-import LeaksCounter from "@/components/ui/cards/LeaksCounter";
+import MapControls from "@/components/ui/map/MapControls";
 
-const Map = dynamic(() => import("@/components/ui/Map"), {
+const Map = dynamic(() => import("@/components/ui/map/Map"), {
   ssr: false
 })
 
 export default function Home() {
   return (
     <main className="relative overflow-hidden">
-      <Map height='calc(100vh - 68px)' />
-      <div className='absolute top-3 right-3 z-[9999999]'>
-        <LeaksCounter count={10} />
-      </div>
+      <Map height='calc(100vh - 68px)' showPointer />
+      <MapControls />
     </main>
   )
 }
