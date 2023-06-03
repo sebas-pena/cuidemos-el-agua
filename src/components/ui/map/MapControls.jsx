3d9406eccfaa59dc3afd02748151f1c49da671cd
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-03 16:22:11 -0300

    feat: Show the file and description inputs

--- src/components/ui/map/MapControls.jsx
+++ src/components/ui/map/MapControls.jsx
@@ -4,15 +4,25 @@ import { useSelector } from 'react-redux'
 import ConfirmLocationButton from '../button/ConfirmLocationButton'
 import LeaksCounter from '../cards/LeaksCounter'
 import ReportButton from '../button/ReportButton'
+import DescriptionInput from '../input/DescriptionInput'
+import FileInput from '../input/FileInput'
 
 const MapControls = () => {
-  const showingCrosshair = useSelector(state => state.map).showCrosshair
+  const { showCrosshairText, showDescriptionInput, showFileInput } = useSelector(state => state.map)
   return (
     <div className='absolute top-3 right-3 z-[9999999]'>
       <LeaksCounter count={10} />
       <ReportButton />
       {
-        showingCrosshair && <ConfirmLocationButton />
+        showCrosshairText && <ConfirmLocationButton />
+      }
+      {
+        showDescriptionInput &&
+        <DescriptionInput />
+      }
+      {
+        showFileInput &&
+        <FileInput />
       }
     </div>
   )
