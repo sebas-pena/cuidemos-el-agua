c0bcd3e8621e9cc798c4a093988eaaaa72f4e46f
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-03 16:21:05 -0300

    feat: Handle 500 error

--- src/app/api/v1/leak/route.js
+++ src/app/api/v1/leak/route.js
@@ -1,23 +1,42 @@
 import { NextResponse } from "next/server"
 import { dbConnection } from "@/db"
+import { Leak } from "@/models/Leak"
 
 export const GET = async (req, res) => {
-  if (!global.db) {
-    await dbConnection()
-    global.db = true
+  try {
+
+    if (!global.db) {
+      await dbConnection()
+      global.db = true
+    }
+    const leaks = await Leak.find({})
+    return NextResponse.json(leaks, { status: 200 })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
   }
 }
 
 export const POST = async (req, res) => {
-  if (!global.db) {
-    await dbConnection()
-    global.db = true
+  try {
+    if (!global.db) {
+      await dbConnection()
+      global.db = true
+    }
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
   }
 }
 
 export const PUT = async (req, res) => {
-  if (!global.db) {
-    await dbConnection()
-    global.db = true
+  try {
+    if (!global.db) {
+      await dbConnection()
+      global.db = true
+    }
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
   }
 }
