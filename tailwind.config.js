22b0029d1e05d2b89dc2811a1c8ada1af42acfbe
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-03 01:50:22 -0300

    feat: Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,20 @@ export const theme = {
         900: '#333333',
         950: '#1a1a1a'
       }
+    },
+    keyframes: {
+      'fade-in': {
+        '0%': { opacity: '0' },
+        '100%': { opacity: '1' }
+      },
+      'fade-out': {
+        '0%': { opacity: '1' },
+        '100%': { opacity: '0' }
+      }
+    },
+    animation: {
+      'fade-in': 'fade-in 0.3s ease-in-out',
+      'fade-out': 'fade-out 0.3s ease-in-out'
     }
   }
 }
