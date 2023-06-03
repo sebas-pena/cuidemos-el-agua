8dd5e760df764abff4bd01cd8894e931d91666f4
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-03 16:20:42 -0300

    feat: Use the button wrapper and the new reducer

--- src/components/ui/button/ConfirmLocationButton.jsx
+++ src/components/ui/button/ConfirmLocationButton.jsx
@@ -1,25 +1,25 @@
 'use client'
-import { hideCrosshairText, lock } from '@/store/feature/MapSlice'
+import { confirmLocation } from '@/store/feature/MapSlice'
 import Image from 'next/image'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import MapControlButton from './MapControlButton'
 
 const ConfirmLocationButton = () => {
   const dispatch = useDispatch()
   const handleClick = () => {
-    dispatch(lock())
-    dispatch(hideCrosshairText())
+    dispatch(confirmLocation())
   }
   return (
-    <button
-      className='flex gap-2 w-full justify-center items-center rounded-lg  py-2 px-4 shadow-md bg-green-600 hover:bg-green-700 active:bg-green-800'
+    <MapControlButton
       onClick={handleClick}
+      styles='green'
     >
       <Image src='/svg/tick.svg' width={20} height={20} alt='Reportar' />
       <span className='font-coolvetica font-semibold text-xl text-white'>
         Confirmar Ubicación
       </span>
-    </button>
+    </MapControlButton>
   )
 }
 
