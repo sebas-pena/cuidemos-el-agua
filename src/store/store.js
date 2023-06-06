1ae33670a4b1f21e764eab2ef02640b1c81628e7
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-06 19:49:43 -0300

    feat: Use the appReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 'use client'
 import { configureStore } from '@reduxjs/toolkit'
 import mapReducer from './feature/MapSlice'
+import appReducer from './feature/AppSlice'
 export const store = configureStore({
   reducer: {
-    map: mapReducer
+    map: mapReducer,
+    app: appReducer
   },
 })
