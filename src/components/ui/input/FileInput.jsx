eac62f072bd4ce679ac02c69145588190c83993d
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-06 19:53:22 -0300

    feat: handle upload reports

--- src/components/ui/input/FileInput.jsx
+++ src/components/ui/input/FileInput.jsx
@@ -2,11 +2,16 @@
 import React, { useState, useRef } from 'react'
 import Image from 'next/image'
 import MapControlButton from '../button/MapControlButton'
-
+import DeleteButton from '../button/DeleteButton'
+import { useSelector, useDispatch } from 'react-redux'
+import { addMarker, sendReport } from '@/store/feature/MapSlice'
+import { increaseTotalReports } from '@/store/feature/AppSlice'
 const FileInput = () => {
   const [file, setFile] = useState(null)
+  const [loading, setLoading] = useState(null)
   const inputRef = useRef(null)
-
+  const dispatch = useDispatch()
+  const { description, center } = useSelector(state => state.map)
   const handleInputChange = (e) => {
     const file = e.target.files[0]
     setFile(file)
@@ -14,38 +19,76 @@ const FileInput = () => {
   const handleUpload = (e) => {
     if (file == null) {
       inputRef.current?.click()
-      console.log(inputRef.current)
     } else {
+      const formData = new FormData()
+      formData.append('file', file)
+      formData.append('description', description)
+      formData.append('lat', center.lat)
+      formData.append('lng', center.lng)
+      setLoading(true)
+      fetch('/api/v1/report', {
+        method: 'POST',
+        body: formData,
+      })
+        .then(res => res.ok ? res.json() : Promise.reject(res))
+        .then(data => {
+          inputRef.current.value = ''
+          setFile(null)
+          dispatch(sendReport())
+          dispatch(increaseTotalReports())
+          dispatch(addMarker(data.report))
+        })
+        .finally(() => {
+          setLoading(false)
+        })
     }
   }
+  const handleDelete = () => {
+    setFile(null)
+    inputRef.current.value = ''
+  }
+
   return (
     <>
-      <label
-        className='block bg-white rounded-lg shadow-md p-2 cursor-pointer group'
-      >
-        <div
-          className='flex flex-col items-center justify-center border-dashed border-2 border-gray-300 rounded-lg h-52 w-full group-hover:border-blue-400 focus-within:border-blue-400'
+      <div className='bg-white rounded-lg shadow-md'>
+
+        <label
+          className={`block p-2 cursor-pointer group ${file != null ? 'hidden' : 'visible'}`}
         >
-          <Image src='/svg/image.svg' width={50} height={50} alt='Agregar Imagen' />
-          <p className='text-[#ccc] font-semibold'>
-            Agrega una imagen
-          </p>
+          <div
+            className='flex flex-col items-center justify-center border-dashed border-2 border-gray-300 rounded-lg aspect-square w-full group-hover:border-blue-400 focus-within:border-blue-400'
+          >
+            <Image src='/svg/image.svg' width={50} height={50} alt='Agregar Imagen' />
+            <p className='text-[#ccc] font-semibold'>
+              Agrega una imagen
+            </p>
+          </div>
+          <input
+            ref={inputRef}
+            type="file"
+            className='hidden'
+            accept='image/*'
+            onChange={handleInputChange}
+            multiple={false}
+          />
+        </label>
+        <div className={`relative w-full aspect-square flex ${file == null ? 'hidden' : 'visible'}`}>
+          <img src={file != null ? URL.createObjectURL(file) : ''} alt='Imagen' className='object-contain flex-1' />
+          <DeleteButton className='absolute top-2 right-2 z-[100000]' onClick={handleDelete} />
         </div>
-        <input
-          ref={inputRef}
-          type="file"
-          className='hidden'
-          accept='image/*'
-          onChange={handleInputChange}
-          multiple={false}
-        />
-      </label>
+      </div>
       <MapControlButton
         style={file != null ? 'green' : 'light-neutral'}
         onClick={handleUpload}
+        disabled={loading}
       >
         <span className='font-coolvetica font-semibold text-xl text-white'>
-          {file != null ? 'Enviar Reporte' : 'Agrega una imagen para continuar'}
+          {
+            loading
+              ? 'Enviando...'
+              : (file != null ? 'Enviar Reporte' : 'Agrega una imagen para continuar')
+          }
+
         </span>
       </MapControlButton>
     </>
