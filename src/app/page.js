793a7b0da241f2354a040a5c5937fd37bb8ae204
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-06 19:54:08 -0300

    feat: Show selected reports

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import MapControls from "@/components/ui/map/MapControls";
+import DisplayReport from "@/components/ui/cards/DisplayReport";
 
 const Map = dynamic(() => import("@/components/ui/map/Map"), {
   ssr: false
@@ -10,6 +11,11 @@ export default function Home() {
     <main className="relative overflow-hidden">
       <Map height='calc(100vh - 68px)' showPointer />
       <MapControls />
+      <div
+        className="absolute top-3 left-3 z-[99999]"
+      >
+        <DisplayReport />
+      </div>
     </main>
   )
 }
