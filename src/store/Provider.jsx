7841b2693815a96c8fd34af6515539c3844d4edb
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-06 19:51:16 -0300

    feat: Use the store initializer component

--- src/store/Provider.jsx
+++ src/store/Provider.jsx
@@ -1,10 +1,12 @@
 'use client'
 import { Provider } from "react-redux";
 import { store } from "./store";
+import StoreInitializer from "@/components/utils/StoreInitializer";
 
 export default function ReduxProvider({ children }) {
   return (
     <Provider store={store}>
+      <StoreInitializer />
       {children}
     </Provider>
   );
