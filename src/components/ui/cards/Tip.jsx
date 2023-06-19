d636efecdbb200b25fa6d641c63dc094f18b5cc4
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-18 21:06:55 -0300

    feat: Improve styles

--- src/components/ui/cards/Tip.jsx
+++ src/components/ui/cards/Tip.jsx
@@ -4,12 +4,12 @@ import Image from 'next/image'
 
 const Tip = ({ title, color, description, img }) => {
   return (
-    <CardWrapper>
-      <div className="flex items-center gap-2">
-        <Image width={50} height={50} src={`/img/${img}`} alt={title} />
+    <CardWrapper padding='p-7'>
+      <div className="flex items-center gap-12 px-4">
+        <Image width={55} height={55} src={`/svg/${img}`} alt={title} />
         <div>
-          <h3 className="text-xl font-bold" style={{ color }}>{title}</h3>
-          <p className="text-gray-500">{description}</p>
+          <h3 className="text-xl font-bold font-coolvetica" style={{ color }}>{title}</h3>
+          <p className="text-gray-500 text-md font-semibold">{description}</p>
         </div>
       </div>
     </CardWrapper>
