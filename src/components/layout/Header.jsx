dce5e58a546e72347ff509a53808508158cac7d8
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-18 21:06:25 -0300

    feat: Show the hamburger icon and add link to homepage

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,25 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
     <header className='w-full bg-white p-4'>
-      <h1 className='flex gap-2 items-center'>
-        <Image src='/svg/logo.svg' alt='logo de cuidemos el agua' width={30} height={30} />
-        <span className='font-coolvetica font-semibold text-3xl text-[#406099]'>
-          Cuidemos el Agua
-        </span>
-        <Image src='/svg/logo-ceibal.svg' alt='logo ceibal' width={26} height={26} />
-      </h1>
+      <div>
+
+        <Link href="/">
+          <h1 className='inline-flex gap-2 items-center'>
+            <Image src='/svg/logo.svg' alt='logo de cuidemos el agua' width={30} height={30} />
+            <span className='font-coolvetica font-semibold text-3xl text-[#406099]'>
+              Cuidemos el Agua
+            </span>
+            <Image src='/svg/logo-ceibal.svg' alt='logo ceibal' width={26} height={26} />
+          </h1>
+        </Link>
+        <div>
+          <Image src='/svg/hamburguer.svg' alt='menu hamburguesa' width={30} height={30} />
+        </div>
+      </div>
     </header>
   )
 }
