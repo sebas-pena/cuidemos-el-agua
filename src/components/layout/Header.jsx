e0eb228446e71aa3825447f4acf33134fc30bfe6
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-23 10:13:09 -0300

    fix: Fix the height of the component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,21 +4,18 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='w-full bg-white p-4'>
+    <header className='w-full flex justify-between items-center px-4 bg-white h-16 overflow-hidden'>
+      <Link href="/">
+        <h1 className='inline-flex gap-2 items-center'>
+          <Image src='/svg/logo.svg' alt='logo de cuidemos el agua' width={30} height={30} />
+          <span className='font-coolvetica font-semibold text-3xl text-[#406099]'>
+            Cuidemos el Agua
+          </span>
+          <Image src='/svg/logo-ceibal.svg' alt='logo ceibal' width={26} height={26} />
+        </h1>
+      </Link>
       <div>
-
-        <Link href="/">
-          <h1 className='inline-flex gap-2 items-center'>
-            <Image src='/svg/logo.svg' alt='logo de cuidemos el agua' width={30} height={30} />
-            <span className='font-coolvetica font-semibold text-3xl text-[#406099]'>
-              Cuidemos el Agua
-            </span>
-            <Image src='/svg/logo-ceibal.svg' alt='logo ceibal' width={26} height={26} />
-          </h1>
-        </Link>
-        <div>
-          <Image src='/svg/hamburguer.svg' alt='menu hamburguesa' width={30} height={30} />
-        </div>
+        <Image className='md:hidden' src='/svg/hamburguer.svg' alt='menu hamburguesa' width={30} height={30} />
       </div>
     </header>
   )
