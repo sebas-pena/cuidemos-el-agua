8ebd2cbfe8478ffb28da6d460425d8b96e99d11a
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-23 15:11:01 -0300

    feat: Add navlinks

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,13 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import ButtonLink from '../ui/link/ButtonLink'
+import SimpleLink from '../ui/link/SimpleLink'
 
 const Header = () => {
+  const user = useSelector(state => state.user)
   return (
     <header className='w-full flex justify-between items-center px-4 bg-white h-16 overflow-hidden'>
       <Link href="/">
@@ -14,8 +19,23 @@ const Header = () => {
           <Image src='/svg/logo-ceibal.svg' alt='logo ceibal' width={26} height={26} />
         </h1>
       </Link>
-      <div>
+      <div className='flex gap-4'>
         <Image className='md:hidden' src='/svg/hamburguer.svg' alt='menu hamburguesa' width={30} height={30} />
+        <SimpleLink href='/noticias'>
+          Noticias
+        </SimpleLink>
+        <SimpleLink href='/consejos'>
+          Consejos
+        </SimpleLink>
+        {
+          user.loggedIn
+            ? <ButtonLink href='/auth/logout'>
+              Cerrar sesión
+            </ButtonLink>
+            : <ButtonLink href='/auth/login'>
+              Iniciar sesión
+            </ButtonLink>
+        }
       </div>
     </header>
   )
