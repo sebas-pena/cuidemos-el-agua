10d720c03ee6b737e171ec7ce8ab0ed336daf81d
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-24 18:58:34 -0300

    feat: Add route to confirm emails

---
+++ src/app/api/v1/auth/confirm-mail/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server"
+import { headers } from 'next/headers'
+import { dbConnection } from "@/db"
+import { User } from "@/models/User"
+import { verifyToken } from "@/utils/server/jwt"
+
+export const POST = async (req) => {
+  if (!global.db) {
+    await dbConnection()
+    global.db = true
+  }
+  const headersList = headers()
+  const token = headersList.get("authorization").split(" ")[1]
+
+  try {
+    const payload = verifyToken(token)
+    await User.updateOne(
+      { email: payload.email },
+      { emailVerified: true },
+      { new: true }
+    )
+    return NextResponse.json({ message: "Email verified" }, { status: 200 })
+  } catch (e) {
+    console.log(e)
+    return NextResponse.json({ message: "Invalid token" }, { status: 401 })
+  }
+}
