34e4bffd5c17f8ca8c2ef1dc2c3c9b6461595513
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-24 18:58:11 -0300

    feat: Implement the functionality

--- src/components/ui/form/LoginForm.jsx
+++ src/components/ui/form/LoginForm.jsx
@@ -5,16 +5,71 @@ import AuthInput from '../input/AuthInput'
 const LoginForm = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
+  const [inputErrors, setInputErrors] = useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { email, password } = e.target
+
+    const emailRegex = /^[A-Za-z0-9](([a-zA-Z0-9,=\.!\-#|\$%\^&\*\+/\?_`\{\}~]+)*)@(?:[0-9a-zA-Z-]+\.)+[a-zA-Z]{2,9}$/
+    const inputErros = {}
+
+    if (!emailRegex.test(email.value)) {
+      inputErros.email = 'Correo electrónico inválido'
+    }
+
+    if (password.value.length == 0) {
+      inputErros.password = 'Ingresa tu contraseña'
+    }
+
+    if (Object.keys(inputErros).length > 0) {
+      setInputErrors(inputErros)
+      return
+    }
+
+    setIsLoading(true)
+    fetch('/api/v1/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: email.value, password: password.value })
+    })
+      .then(async res => {
+        const data = await res.json()
+        if (res.ok) {
+          return data
+        } else {
+          const error = new Error(data.message || 'Something went wrong!')
+          throw error
+        }
+      })
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
 
   return (
-    <form className='relative flex flex-col gap-7'>
+    <form
+      className='relative flex flex-col w-full max-w-lg gap-7 mb-5'
+      onSubmit={handleSubmit}
+    >
       <AuthInput
         title='Correo electrónico'
-        type='email'
+        error={inputErrors.email}
+        name='email'
       />
       <AuthInput
         title='Contraseña'
         type='password'
+        error={inputErrors.password}
+        name='password'
       />
       <button
         type='submit'
--- src/components/ui/form/RegisterForm.jsx
+++ src/components/ui/form/RegisterForm.jsx
@@ -4,11 +4,13 @@ import React, { useState } from 'react'
 import AuthInput from '../input/AuthInput'
 import PhoneInput from '../input/PhoneInput'
 import Image from 'next/image'
+import EmailModal from '../modal/EmailModal'
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
   const [inputsErrors, setInputsErrors] = useState({})
+  const [showSendEmailModal, setShowSendEmailModal] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -56,51 +58,92 @@ const RegisterForm = () => {
     }
 
     setInputsErrors({})
-
     setIsLoading(true)
+
+    fetch('/api/v1/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value,
+        phone: phoneNumber.value
+      })
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('Error al crear la cuenta')
+        }
+      })
+      .then(data => {
+        console.log(data)
+        setShowSendEmailModal(true)
+        setError(null)
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  const handleCloseModal = () => {
+    setShowSendEmailModal(false)
   }
 
   return (
-    <form
-      className='relative flex flex-col gap-1'
-      onSubmit={handleSubmit}
-    >
-      <AuthInput
-        title='Correo electrónico'
-        type='email'
-        name='email'
-        error={inputsErrors.email}
-      />
-      <AuthInput
-        title='Contraseña'
-        type='password'
-        name='password'
-        error={inputsErrors.password}
-      />
-      <AuthInput
-        title='Confirmar Contraseña'
-        type='password'
-        name='confirm-password'
-        error={inputsErrors.confirmPassword}
-      />
-      <PhoneInput
-        title='Numero de telefono'
-        name='phone-number'
-        error={inputsErrors.phoneNumber}
-      />
-      <button
-        type='submit'
-        className="text-lg rounded-md h-9 flex justify-center items-center px-4 mt-5 text-white font-coolvetica bg-blue-600 active:bg-blue-800 hover:bg-blue-700 duration-300 disabled:bg-blue-400 "
-        disabled={isLoading}
+    <>
+      <form
+        className='relative flex flex-col w-full gap-1 max-w-lg mx-auto px-4 pb-4'
+        onSubmit={handleSubmit}
       >
+        <AuthInput
+          title='Correo electrónico'
+          name='email'
+          error={inputsErrors.email}
+        />
+        <AuthInput
+          title='Contraseña'
+          type='password'
+          name='password'
+          error={inputsErrors.password}
+        />
+        <AuthInput
+          title='Confirmar Contraseña'
+          type='password'
+          name='confirm-password'
+          error={inputsErrors.confirmPassword}
+        />
+        <PhoneInput
+          title='Numero de telefono'
+          name='phone-number'
+          error={inputsErrors.phoneNumber}
+        />
+        <button
+          type='submit'
+          className="text-lg rounded-md h-9 flex justify-center items-center px-4 mt-5 text-white font-coolvetica bg-blue-600 active:bg-blue-800 hover:bg-blue-700 duration-300 disabled:bg-blue-400 "
+          disabled={isLoading}
+        >
+          {
+            isLoading ? <Image src='/svg/spinner.svg' alt='loading' width={24} height={24} /> : 'Crear Cuenta'
+          }
+        </button>
         {
-          isLoading ? <Image src='/svg/spinner.svg' alt='loading' width={24} height={24} /> : 'Crear Cuenta'
+          error && <p className='w-full font-semibold absolute top-full text-red-500 text-center'>{error}</p>
         }
-      </button>
+      </form>
       {
-        error && <p className='w-full font-semibold absolute top-full translate-y-3 text-red-500 text-center'>{error}</p>
+        showSendEmailModal && (
+          <div className='absolute w-full h-screen top-0 left-0'>
+            <EmailModal handleCloseModal={handleCloseModal} />
+          </div>
+        )
       }
-    </form>
+    </>
   )
 }
 
