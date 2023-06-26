f6894fc82270a258d50699c7f3734654329c5558
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-26 00:13:26 -0300

    feat: Improve component

--- src/components/ui/link/SimpleLink.jsx
+++ src/components/ui/link/SimpleLink.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 
-const SimpleLink = ({ children, paddingX = 'px-4', paddingY = 'py-1', ...props }) => {
+const SimpleLink = ({ children, paddingX = 'px-4', paddingY = 'py-1', fontSize = 'text-lg', ...props }) => {
   return (
-    <Link {...props} className={`text-lg rounded-md ${paddingY} ${paddingX} font-coolvetica text-blue-600 active:text-blue-800 hover:text-blue-700 duration-300`}>
+    <Link {...props} className={`${fontSize} rounded-md ${paddingY} ${paddingX} font-coolvetica text-blue-600 active:text-blue-800 hover:text-blue-700 duration-300`}>
       {children}
     </Link>
   )
