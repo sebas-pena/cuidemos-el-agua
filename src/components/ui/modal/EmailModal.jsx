bbae17e45ace8daa1e98dce7f27adbeca08e6290
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-26 00:07:49 -0300

    feat: Add modals

--- src/components/ui/modal/EmailModal.jsx
+++ src/components/ui/modal/EmailModal.jsx
@@ -1,20 +1,24 @@
 'use client'
 import React from 'react'
 import CardWrapper from '../cards/Wrapper'
+import ButtonLink from '../link/ButtonLink'
+import CloseButton from '../button/CloseButton'
 
-const EmailModal = ({ handleCloseModal }) => {
+const EmailModal = ({ handleCloseModal, emailProvider }) => {
   return (
     <div className='w-full h-full flex items-center justify-center bg-black/30'>
       <CardWrapper padding='px-8 py-7'>
-        <div className='flex flex-col justify-center items-center gap-2'>
-          <h1 className='text-2xl max-w-sm font-coolvetica text-neutral-800'>Verifica tu correo electrónico</h1>
-          <p className='text-center text-neutral-700'>Te hemos enviado un correo electrónico con un enlace para verificar tu cuenta.</p>
-          <button
-            className='text-md rounded-md h-8 flex justify-center items-center px-4 mt-5 text-white font-coolvetica bg-blue-600 active:bg-blue-800 hover:bg-blue-700 duration-300'
-            onClick={handleCloseModal}
-          >
-            Cerrar
-          </button>
+        <div className='relative'>
+          <div className='absolute right-0 translate-x-4 -translate-y-3 top-0'>
+            <CloseButton onClick={handleCloseModal} />
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <h1 className='text-2xl max-w-sm font-coolvetica text-neutral-800'>Verifica tu correo electrónico</h1>
+            <p className='text-center text-neutral-700 mb-3'>Te hemos enviado un correo electrónico con un enlace para verificar tu cuenta.</p>
+            <ButtonLink href={`https://${emailProvider}`} paddingX='px-0' paddingY='py-0' className='text-md rounded-md h-8 flex justify-center items-center px-4 mt-5 text-white font-coolvetica bg-blue-600 active:bg-blue-800 hover:bg-blue-700 duration-300'>
+              Ir a mi casilla de correo
+            </ButtonLink>
+          </div>
         </div>
       </CardWrapper>
     </div>
---
+++ src/components/ui/modal/ErrorConfirmEmail.jsx
@@ -0,0 +1,76 @@
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import CardWrapper from '../cards/Wrapper'
+import CloseButton from '../button/CloseButton'
+
+const ErrorConfirmEmail = ({ handleCloseModal, resendEmail, credentials }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [emailSented, setEmailSented] = useState(false)
+
+  const handleResendEmail = () => {
+    setIsLoading(true)
+    fetch('/api/v1/auth/resend-confirmation-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(async res => {
+        const data = await res.json()
+        if (res.ok) {
+          return data
+        } else {
+          const error = new Error(data.message || 'Something went wrong!')
+          throw error
+        }
+      })
+      .then(data => {
+        setEmailSented(true)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  return (
+    <div className='w-full h-full flex items-center justify-center bg-black/30'>
+      <div className='max-w-2xl text-center'>
+        <CardWrapper padding='px-8 py-7' width="w-full">
+          <div className='relative'>
+            <div className='absolute right-0 translate-x-4 -translate-y-3 top-0'>
+              <CloseButton onClick={handleCloseModal} />
+            </div>
+            <div className='flex flex-col justify-center items-center gap-2'>
+              <h3 className='text-2xl max-w-sm font-coolvetica text-neutral-800'>¡Ups! Verifica tu correo electrónico</h3>
+              <p className='text-neutral-700'>
+                Antes de continuar, debes verificar tu correo electrónico. Revisa tu casilla de correo y haz click en el enlace que te enviamos.
+              </p>
+            </div>
+            {
+              !resendEmail && (
+                <button
+                  type='submit'
+                  className={`text-lg rounded-md h-9 flex justify-center items-center px-4 mt-4 mx-auto text-white font-coolvetica bg-blue-600 active:bg-blue-800 hover:bg-blue-700 duration-300 ${emailSented ? "" : "disabled:bg-blue-400"}`}
+                  disabled={isLoading || emailSented}
+                  onClick={handleResendEmail}
+                >
+                  {
+                    isLoading ? <Image src='/svg/spinner.svg' alt='loading' width={24} height={24} />
+                      : emailSented ? "Correo Enviado" : 'Reenviar correo electrónico de confirmación'
+                  }
+                </button>
+              )
+            }
+          </div>
+        </CardWrapper>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorConfirmEmail
