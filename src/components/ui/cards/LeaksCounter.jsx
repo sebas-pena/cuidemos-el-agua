e57bc4c00160fe69fbf710c31a9f17b44dba76e4
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-26 00:09:14 -0300

    fix: Use the leakslice, the new cards props, add responsive design

--- src/components/ui/cards/LeaksCounter.jsx
+++ src/components/ui/cards/LeaksCounter.jsx
@@ -1,19 +1,29 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import CardWrapper from './Wrapper'
+import { useSelector } from 'react-redux'
 
-const LeaksCounter = ({ count }) => {
+const LeaksCounter = () => {
+  const { totalLeaksCounter } = useSelector(state => state.leaks)
   return (
-    <CardWrapper>
+    <CardWrapper
+      hideShadownMobile
+      noRoundedMobile
+      padding='p-2 py-4 md:px'
+      className="flex-1"
+    >
       <div className='flex gap-5'>
         <Image src='/svg/leak.svg' alt='icono de perdida de agua' width={45} height={45} />
         <div className='flex flex-col'>
-          <h2 className='text-lg font-coolvetica font-semibold text-mine-shaft-600'>
-            Perdidas Reportadas Hoy
+          <h2 className='text-sm md:text-lg font-coolvetica font-semibold text-mine-shaft-600'>
+            Perdidas Reportadas
           </h2>
-          <span className='text-2xl font-coolvetica font-semibold text-mine-shaft-600 text-center'>
-            {count}
-          </span>
+          {
+            totalLeaksCounter === null
+              ? <Image src='/svg/spinner-black.svg' alt='loading' className='mx-auto py-[7px]' width={24} height={24} />
+              : <span className='text-2xl font-coolvetica font-semibold text-mine-shaft-600 text-center'>{totalLeaksCounter}</span>
+          }
         </div>
       </div>
     </CardWrapper>
