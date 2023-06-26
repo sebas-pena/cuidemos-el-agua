78d2e202d79cf13c8e4e5b416984450dcd904a25
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-25 23:59:31 -0300

    refactor: Improve code and return user in the response

--- src/app/api/v1/auth/login/route.js
+++ src/app/api/v1/auth/login/route.js
@@ -5,29 +5,54 @@ import { NextResponse } from "next/server"
 
 export const POST = async (req) => {
   const { email, password } = await req.json()
-
   const emailRegex = /^[A-Za-z0-9](([a-zA-Z0-9,=\.!\-#|\$%\^&\*\+/\?_`\{\}~]+)*)@(?:[0-9a-zA-Z-]+\.)+[a-zA-Z]{2,9}$/
 
+  const BAD_CREDENTIALS_RESPONSE = NextResponse.json(
+    {
+      message: "Bad credentials",
+      code: 0
+    }
+    ,
+    { status: 401 }
+  )
+
   if (!emailRegex.test(email) || password.length < 8) {
-    console.log("Invalid password or email")
-    return NextResponse.json({ message: "Bad credentials" }, { status: 401 })
+    return BAD_CREDENTIALS_RESPONSE
   }
 
   const user = await User.findOne({ email })
 
   if (!user) {
-    console.log("User not found")
-    return NextResponse.json({ message: "Bad credentials" }, { status: 401 })
+    return BAD_CREDENTIALS_RESPONSE
   }
 
   const isPasswordValid = await comparePassword(password, user.password)
 
   if (!isPasswordValid) {
-    console.log("Invalid password")
-    return NextResponse.json({ message: "Bad credentials" }, { status: 401 })
+    return BAD_CREDENTIALS_RESPONSE
   }
 
-  const res = NextResponse.json({ message: "Logged in" }, { status: 200 })
+  if (!user.emailVerified) {
+    return NextResponse.json({
+      message: "Email not verified",
+      code: 1
+    },
+      { status: 401 }
+    )
+  }
+
+  const res = NextResponse.json({
+    user: {
+      email: user.email,
+      emailVerified: user.emailVerified,
+      phone: user.phone,
+      phoneVerified: user.phoneVerified,
+    },
+    message: "Logged in"
+  },
+    { status: 200 }
+  )
+
   const token = createToken({
     email: user.email,
     emailVerified: user.emailVerified,
@@ -35,7 +60,7 @@ export const POST = async (req) => {
     phoneVerified: user.phoneVerified,
   })
 
-  res.cookies.set("token", token, {
+  res.cookies.set("auth-token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 60 * 60 * 24 * 30,
