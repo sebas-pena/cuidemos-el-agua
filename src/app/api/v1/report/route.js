1c8ca0ad1a0a5b1cc8359e34feae5476e589f499
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-27 01:56:36 -0300

    feat: Implement authenticated users-only report

--- src/app/api/v1/report/route.js
+++ src/app/api/v1/report/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server"
 import { dbConnection } from "@/db"
 import { Leak } from "@/models/Leak"
 import { uploadReport } from "@/utils/server/discord"
+import { cookies } from "next/headers"
+import { verifyToken } from "@/utils/server/jwt"
 
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url)
@@ -52,41 +54,56 @@ export const GET = async (req) => {
 
 export const POST = async (req) => {
   try {
+
     if (!global.db) {
       await dbConnection()
       global.db = true
     }
-    const form = await req.formData()
-    const image = form.get("file")
-    const description = form.get("description")
-    const location = {
-      lat: parseFloat(form.get("lat")),
-      lng: parseFloat(form.get("lng")),
-    }
-    const leak = new Leak({
-      description,
-      location,
-      image,
-    })
-    const format = image.name.split(".").pop()
-    const result = await uploadReport({
-      location,
-      image,
-      description,
-      filename: `${leak._id}.${format}`,
-      contentType: image.type,
-    })
 
-    if (result.status !== 200) {
-      return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
-    } else {
-      const imageUrl = (await result.json()).attachments[0].url
-      leak.image = imageUrl
-      await leak.save()
+    const cookiesList = cookies()
+    const token = cookiesList.get("auth-token")
+
+    if (!token) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 401 })
     }
-    return NextResponse.json({
-      report: leak,
-    }, { status: 200 })
+
+    try {
+      const payload = verifyToken(token.value)
+      const form = await req.formData()
+      const image = form.get("file")
+      const description = form.get("description")
+      const location = {
+        lat: parseFloat(form.get("lat")),
+        lng: parseFloat(form.get("lng")),
+      }
+      const leak = new Leak({
+        description,
+        location,
+        image,
+      })
+      const format = image.name.split(".").pop()
+      const result = await uploadReport({
+        location,
+        image,
+        description,
+        filename: `${leak._id}.${format}`,
+        contentType: image.type,
+      })
+
+      if (result.status !== 200) {
+        return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
+      } else {
+        const imageUrl = (await result.json()).attachments[0].url
+        leak.image = imageUrl
+        await leak.save()
+      }
+      return NextResponse.json({
+        report: leak,
+      }, { status: 200 })
+    } catch (e) {
+      console.log(e)
+      return NextResponse.json({ error: "Invalid token" }, { status: 401 })
+    }
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
