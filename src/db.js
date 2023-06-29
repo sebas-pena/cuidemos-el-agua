a12eac9ef0f32569adcba615d0cd9f6e0edbd5a0
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-29 02:58:28 -0300

    refactor: Checks if a previous connection exists in the connection function

--- src/db.js
+++ src/db.js
@@ -1,6 +1,11 @@
 import { connect } from "mongoose";
 
-export const dbConnection = () => connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+export const dbConnection = () => {
+  if (!global.db) {
+    global.db = true;
+    return connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  }
+}
