579db523fdfcc1ac8bced4b4a1c27d1ba0da1764
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-06-29 21:14:13 -0300

    feat: Show OSE twits

--- src/app/(with-header)/noticias/page.js
+++ src/app/(with-header)/noticias/page.js
@@ -1,8 +1,11 @@
+import OSEProfile from '@/components/ui/twitter/OSEProfile'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='p-5 max-w-3xl mx-auto'>
+      <OSEProfile />
+    </div>
   )
 }
 
---
+++ src/components/ui/twitter/OSEProfile.jsx
@@ -0,0 +1,34 @@
+'use client'
+import React, { useEffect } from 'react'
+
+const OSEProfile = () => {
+  useEffect(() => {
+    const s = document.createElement("script");
+    s.setAttribute("src", "https://platform.twitter.com/widgets.js");
+    s.setAttribute("async", "true");
+    document.head.appendChild(s);
+  }, [])
+
+  const screenWidth = window.innerWidth
+  let embedWidth = 550
+
+  if (screenWidth < 580) {
+    embedWidth = screenWidth - 30
+    if (embedWidth < 220) {
+      embedWidth = 220
+    }
+  }
+  return (
+    <a
+      className="twitter-timeline"
+      data-width={embedWidth}
+      data-dnt="true"
+      data-theme="light"
+      data-tweet-limit="10"
+      data-chrome="noheader"
+      href="https://twitter.com/OSE_Uruguay?ref_src=twsrc%5Etfw">
+    </a>
+  )
+}
+
+export default OSEProfile
