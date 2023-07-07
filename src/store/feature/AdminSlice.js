a9cee844eec8e914aee278c1c22fe142675c93da
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-07-07 14:46:33 -0300

    feat: Add AdminSlice

---
+++ src/store/feature/AdminSlice.js
@@ -0,0 +1,17 @@
+'use client'
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+}
+
+export const admin = createSlice({
+  name: 'admin',
+  initialState,
+  reducers: {
+  }
+})
+
+export const {
+
+} = admin.actions
+export default admin.reducer
