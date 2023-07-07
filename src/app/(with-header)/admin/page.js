ab727092fdbfd33b088d3e5dac6d9d4cc4ad2284
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-07-07 14:34:51 -0300

    feat: Add pages to be implemented

---
+++ src/app/(with-header)/admin/page.js
@@ -0,0 +1,10 @@
+import AdminNavBar from "@/components/layout/AdminNavBar"
+
+const page = () => {
+  return (
+    <main className='mx-auto max-w-4xl w-full pt-5'>
+    </main>
+  )
+}
+
+export default page
