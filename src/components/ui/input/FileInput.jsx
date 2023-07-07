ee015373d4e163edc8b73e10966fd1e55a0c5165
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-07-07 14:45:25 -0300

    feat: Accept images and videos as inputs

--- src/components/ui/input/FileInput.jsx
+++ src/components/ui/input/FileInput.jsx
@@ -10,13 +10,24 @@ import { addLeak, increaseTotalLeaksCounter } from '@/store/feature/LeaksSlice'
 const FileInput = () => {
   const [file, setFile] = useState(null)
   const [loading, setLoading] = useState(null)
+  const [fileLimitExceeded, setFileLimitExceeded] = useState(false)
+
   const inputRef = useRef(null)
   const dispatch = useDispatch()
   const { description, center } = useSelector(state => state.map)
+
   const handleInputChange = (e) => {
     const file = e.target.files[0]
+    const fileSizeMB = file.size / 1024 / 1024
+    if (fileSizeMB > 8) {
+      inputRef.current.value = ''
+      setFileLimitExceeded(true)
+      return
+    }
+    setFileLimitExceeded(false)
     setFile(file)
   }
+
   const handleUpload = (e) => {
     if (file == null) {
       inputRef.current?.click()
@@ -44,6 +55,7 @@ const FileInput = () => {
         })
     }
   }
+
   const handleDelete = () => {
     setFile(null)
     inputRef.current.value = ''
@@ -61,22 +73,38 @@ const FileInput = () => {
           >
             <Image src='/svg/image.svg' width={50} height={50} alt='Agregar Imagen' />
             <p className='text-[#ccc] font-semibold'>
-              Agrega una imagen
+              Agrega una imagen o video
+            </p>
+            <p className='text-[#ccc] font-semibold'>
+              .jpg, .jpeg, .png, .mp4
+            </p>
+            <p className={`${fileLimitExceeded ? 'text-red-700 font-semibold' : 'text-[#ccc]'}`}>
+              Máximo 8MB
             </p>
           </div>
           <input
             ref={inputRef}
             type="file"
             className='hidden'
-            accept='image/*'
+            accept='.jpg,.jpeg,.png,.mp4'
             onChange={handleInputChange}
             multiple={false}
           />
         </label>
-        <div className={`relative w-full aspect-square flex ${file == null ? 'hidden' : 'visible'}`}>
-          <img src={file != null ? URL.createObjectURL(file) : ''} alt='Imagen' className='object-contain flex-1' />
-          <DeleteButton className='absolute top-2 right-2 z-[100000]' onClick={handleDelete} />
-        </div>
+        {
+          file != null &&
+          <div className="relative w-full aspect-square flex">
+            {
+              file.type.includes('video') &&
+              <video src={file != null ? URL.createObjectURL(file) : ''} className='object-contain flex-1' controls />
+            }
+            {
+              file.type.includes('image') &&
+              <img src={file != null ? URL.createObjectURL(file) : ''} alt='Imagen' className='object-contain flex-1' />
+            }
+            <DeleteButton className='absolute top-2 right-2 z-[100000]' onClick={handleDelete} />
+          </div>
+        }
       </div>
       <MapControlButton
         style={file != null ? 'green' : 'light-neutral'}
